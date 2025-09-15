d41bee14e61710c30ba9b2c87dc3b3ee257510b7
Behnejad <3690943+behnejad@users.noreply.github.com>
2025-09-15 16:25:09 +0330

    Create helper.sql

---
+++ timescale/helper.sql
@@ -0,0 +1,16 @@
+CREATE USER your_new_username WITH PASSWORD 'your_secure_password';
+GRANT CONNECT ON DATABASE your_database_name TO your_new_username;
+GRANT USAGE ON SCHEMA public TO your_new_username;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO your_new_username;
+GRANT ALL PRIVILEGES ON DATABASE "test_database" to tester;
+SELECT show_chunks('settlementservices-bad', newer_than => '-infinity', older_than => 'infinity');
+select * from _timescaledb_internal._hyper_16_2639_chunk limit 10;
+SELECT create_hypertable('conditions', by_range('time'));
+CREATE DATABASE tablename
+    OWNER = username
+    TEMPLATE = template0
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8';
+ALTER DATABASE databasename OWNER TO usermane;
+sudo -u postgres createuser --interactive --pwprompt
