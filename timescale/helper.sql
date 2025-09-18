c496560e2f9d7fc0fd5e593d6ac1c689b943bfbd
Behnejad <3690943+behnejad@users.noreply.github.com>
2025-09-18 11:50:45 +0330

    Update helper.sql

--- timescale/helper.sql
+++ timescale/helper.sql
@@ -5,7 +5,11 @@ GRANT SELECT ON ALL TABLES IN SCHEMA public TO your_new_username;
 GRANT ALL PRIVILEGES ON DATABASE "test_database" to tester;
 SELECT show_chunks('settlementservices-bad', newer_than => '-infinity', older_than => 'infinity');
 select * from _timescaledb_internal._hyper_16_2639_chunk limit 10;
+
 SELECT create_hypertable('conditions', by_range('time'));
+SELECT create_hypertable('my_table', 'time');
+CREATE INDEX IF NOT EXISTS usage_metric ON my_table (metric, time DESC);
+
 CREATE DATABASE tablename
     OWNER = username
     TEMPLATE = template0
@@ -13,4 +17,9 @@ CREATE DATABASE tablename
     LC_COLLATE = 'en_US.UTF-8'
     LC_CTYPE = 'en_US.UTF-8';
 ALTER DATABASE databasename OWNER TO usermane;
+
+\c my_db
+CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
+
+command: postgres -c shared_preload_libraries=timescaledb
 sudo -u postgres createuser --interactive --pwprompt
